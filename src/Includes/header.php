3fe2ff44f6aa7abe785726a5c64bef903b4521fb
NoahWGeorge <georgen1@etown.edu>
2022-11-07 10:51:03 -0500

    made login, put header code in header file, fixed some minor bugs

---
+++ src/Includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LetDoThis/WorkoutRoutines</title>
+    <link rel="stylesheet" href="css/WorkoutRoutines.css">
+</head>
+<body>
+    <header>
+        <div id="title">
+            <div id='menu' class="dropdown">
+                <a>
+                    <img class='icon' src="./img/menu-white.png">
+                </a>
+            </div>
+            <h1>Lets Do This!</h1>
+            <div id='user' class="dropdown">
+                <a>
+                    <span id='welcome' class="dropbtn">Welcome, <span id='username'>User!</span></span>
+                    <img class='icon' src="/Images/UserIcon.png">
+                </a>
+                <div class="dropdown-content">
+                    <a href="">Log in</a>
+                    <a href="#">Log out</a>
+                </div>
+            </div>
+        </div>
+        <nav class = "h2">
+            <a href="home">Home</a>
+            <a href="groceryList">Grocery List</a>
+            <a href="Calendar">Calendar</a>
+            <a href="Recipes">Recipes</a>
+            <a href="WorkoutRoutines">Workout Routines</a>
+        </nav>
+    </header>

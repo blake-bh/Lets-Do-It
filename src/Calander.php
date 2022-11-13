fa6b985db6d96f5f8d2c4d83295643ff3288679e
Blake Hildebrand <hildebrandb1@etown.edu>
2022-11-13 13:42:43 -0500

    rename all files

---
+++ src/Calander.php
@@ -0,0 +1,16 @@
+<?php
+require('includes/header.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Calender.css">
+</head>
+<body>
+    
+</body>
+</html>

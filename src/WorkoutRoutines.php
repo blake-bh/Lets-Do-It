3a792a1a87b6a4227e69687230a8e745efa3e9e2
NoahWGeorge <georgen1@etown.edu>
2022-12-07 13:29:42 -0500

    adding tables

--- src/WorkoutRoutines.php
+++ src/WorkoutRoutines.php
@@ -19,50 +19,10 @@
 require('Includes/header.html');
 ?>
 
-    <h2>Choose Table Size</h2>
-<select name="table size" id="table">
-  <option value="7x1">7 x 1</option>
-  <option value="7x2">7 x 2</option>
-  <option value="7x3">7 x 3</option>
-  <option value="7x4">7 x 4</option>
-</select>
-        
-        <table border="5" cellspacing="0" align="center">
+    <h2 id=beginner>Beginner Table</h2>
+    <table border="5" cellspacing="0" align="center">
             <!--<caption>Timetable</caption>-->
-            <tr>
-                <td align="center" height="50"
-                    width="100"><br>
-                    <b>workout/split</b></br>
-                </td>
-                <td align="center" height="50"
-                    width="100">
-                    <b>PUSH<br></b>
-                </td>
-                <td align="center" height="50"
-                    width="100">
-                    <b>PULL<br></b>
-                </td>
-                <td align="center" height="50"
-                    width="100">
-                    <b>PUSH<br></b>
-                </td>
-                <td align="center" height="50"
-                    width="100">
-                    <b>PULL<br></b>
-                </td>
-                <td align="center" height="50"
-                    width="100">
-                    <b>PUSH<br></b>
-                </td>
-                <td align="center" height="50"
-                    width="100">
-                    <b>PULL<br></b>
-                </td>
-                <td align="center" height="50"
-                    width="100">
-                    <b>BONUS DAY<br>Upper Body</b>
-                </td>
-            </tr>
+        
             <tr>
                 <td align="center" height="50">
                     <b>Monday</b></td>
@@ -70,9 +30,21 @@ require('Includes/header.html');
                 <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
                 <td align="center" height="50">Shoulders, Chest, Triceps</td>
                 <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
-                <td align="center" height="50">Quad, Chest, Abs</td>
+
+                <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Tuesday</b></td>
+                <td align="center" height="50">Quad, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
                 <td align="center" height="50">Shoulders, Chest, Triceps</td>
                 <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                
 
                 <td rowspan="1" align="center" height="50">
                     
@@ -83,14 +55,12 @@ require('Includes/header.html');
             </tr>
             <tr>
                 <td align="center" height="50">
-                    <b>Tuesday</b></td>
+                    <b>Wednesday</b></td>
                 <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
                 <td align="center" height="50">Quad, Chest, Triceps</td>
                 <td align="center" height="50">Quad, Chest, Abs</td>
                 <td align="center" height="50">Shoulders, Chest, Triceps</td>
-                <td align="center" height="50">Biceps, Triceps, Abs, Shoulders</td>
-                <td align="center" height="50">Quad, Chest, Triceps</td>
-                <td align="center" height="50">Shoulders, Chest, Triceps</td>
+               
                 <td rowspan="1" align="center" height="50">
                 </td>
                 <td colspan="1" align="center"
@@ -99,30 +69,12 @@ require('Includes/header.html');
             </tr>
             <tr>
                 <td align="center" height="50">
-                    <b>Wednesday</b>
+                    <b>Thursday</b>
                     <td align="center" height="50">Quad, Chest, Triceps</td>
                     <td align="center" height="50">Quad, Chest, Abs</td>
                     <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
                     <td align="center" height="50">ALL Lower body</td>
-                    <td align="center" height="50">All back</td>
-                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
-                    <td align="center" height="50">All upper body</td>
-                    <td rowspan="1" align="center" height="50">
-                    </td>
-                    <td colspan="1" align="center"
-                        height="50"></td>
-                    <td align="center" height="50"></td>
-            </tr>
-            <tr>
-                <td align="center" height="50">
-                    <b>Thursday</b>
-                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
-                    <td align="center" height="50">Shoulders, Chest, Triceps</td>
-                    <td align="center" height="50">Quad, Chest, Abs</td>
-                    <td align="center" height="50">Biceps, Triceps, Abs, Shoulders</td>
-                    <td align="center" height="50">Hamstrings, Glutes, Back</td>
-                    <td align="center" height="50">Quad, Chest, Abs</td>
-                    <td align="center" height="50">Hamstrings, Glutes, Back</td>
+                    
                     <td rowspan="1" align="center" height="50">
                     </td>
                     <td colspan="1" align="center"
@@ -132,13 +84,25 @@ require('Includes/header.html');
             <tr>
                 <td align="center" height="50">
                     <b>Friday</b>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    <td align="center" height="50">Biceps, Triceps, Abs, Shoulders</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Saturday</b>
                     <td align="center" height="50">Hamstrings, Glutes, Back</td>
                     <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
                     <td align="center" height="50">Shoulders, Chest, Triceps</td>
                     <td align="center" height="50">Quad, Chest, Abs</td>
-                    <td align="center" height="50">All upper body</td>
-                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
-                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    
                     <td rowspan="1" align="center" height="50">
                     </td>
                     <td colspan="1" align="center"
@@ -148,14 +112,12 @@ require('Includes/header.html');
             </tr>
             <tr>
                 <td align="center" height="50">
-                    <b>Saturday</b>
+                    <b>Sunday</b>
                     <td align="center" height="50">Shoulders, Chest, Triceps</td>
                     <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
                     <td align="center" height="50">Quad, Chest, Abs</td>
                     <td align="center" height="50">All upper body</td>
-                    <td align="center" height="50">Hamstrings, Glutes, Back</td>
-                    <td align="center" height="50">Quad, Chest, Abs</td>
-                    <td align="center" height="50">Hamstrings, Glutes, Back</td>
+                    
                     <td rowspan="1" align="center" height="50">
                     </td>
                     <td colspan="1" align="center"
@@ -163,9 +125,231 @@ require('Includes/header.html');
                     <td align="center" height="50"></td>
         </tr>
     </table>
-    
-    <button id="clear">clear</button>
-    <button id="share">share</button>
+    <h3 id=inter>Intermediate Table</h2>
+    <table border="5" cellspacing="0" align="center">
+            <!--<caption>Timetable</caption>-->
+        
+            <tr>
+                <td align="center" height="50">
+                    <b>Monday</b></td>
+                <td align="center" height="50">Quad, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+
+                <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Tuesday</b></td>
+                <td align="center" height="50">Quad, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                
+
+                <td rowspan="1" align="center" height="50">
+                    
+                </td>
+                <td colspan="1" align="center"
+                    height="50"></td>
+                <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Wednesday</b></td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                <td align="center" height="50">Quad, Chest, Triceps</td>
+                <td align="center" height="50">Quad, Chest, Abs</td>
+                <td align="center" height="50">Shoulders, Chest, Triceps</td>
+               
+                <td rowspan="1" align="center" height="50">
+                </td>
+                <td colspan="1" align="center"
+                    height="50"></td>
+                <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Thursday</b>
+                    <td align="center" height="50">Quad, Chest, Triceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">ALL Lower body</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Friday</b>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    <td align="center" height="50">Biceps, Triceps, Abs, Shoulders</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Saturday</b>
+                    <td align="center" height="50">Hamstrings, Glutes, Back</td>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+                
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Sunday</b>
+                    <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    <td align="center" height="50">All upper body</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+        </tr>
+    </table>
+    <h4 id=expert>Expert Table</h4>
+    <table border="5" cellspacing="0" align="center">
+            <!--<caption>Timetable</caption>-->
+            <tr>
+                <td align="center" height="50">
+                    <b>Monday</b></td>
+                <td align="center" height="50">Quad, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+
+                <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Tuesday</b></td>
+                <td align="center" height="50">Quad, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                
+
+                <td rowspan="1" align="center" height="50">
+                    
+                </td>
+                <td colspan="1" align="center"
+                    height="50"></td>
+                <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Wednesday</b></td>
+                <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                <td align="center" height="50">Quad, Chest, Triceps</td>
+                <td align="center" height="50">Quad, Chest, Abs</td>
+                <td align="center" height="50">Shoulders, Chest, Triceps</td>
+               
+                <td rowspan="1" align="center" height="50">
+                </td>
+                <td colspan="1" align="center"
+                    height="50"></td>
+                <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Thursday</b>
+                    <td align="center" height="50">Quad, Chest, Triceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">ALL Lower body</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Friday</b>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    <td align="center" height="50">Biceps, Triceps, Abs, Shoulders</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Saturday</b>
+                    <td align="center" height="50">Hamstrings, Glutes, Back</td>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+                
+            </tr>
+            <tr>
+                <td align="center" height="50">
+                    <b>Sunday</b>
+                    <td align="center" height="50">Shoulders, Chest, Triceps</td>
+                    <td align="center" height="50">Hamstrings, Glutes, Back, Biceps</td>
+                    <td align="center" height="50">Quad, Chest, Abs</td>
+                    <td align="center" height="50">All upper body</td>
+                    
+                    <td rowspan="1" align="center" height="50">
+                    </td>
+                    <td colspan="1" align="center"
+                        height="50"></td>
+                    <td align="center" height="50"></td>
+        </tr>
+    </table>
+    <h5 id=workoutsearch>Workouts</h5>
+
+    <div id= "workoutsearchbtn">
+        <form>
+            <input type="text" placeholder="Enter a body part" id="ingredient">
+            <input type="button" id="ingredientSubmit" value= "Submit">
+        </form>
+
+    </div>
+    <div >
+        <ul class= 'list'>
+            
+        </ul>
+    </div>
     
 </body>
 </html>

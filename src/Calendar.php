70783fb94a9071a597c102f53f7e73e1aa48de9a
Seamus Corr <corrs@etown.edu>
2022-12-07 20:25:13 -0500

    Added Calendar

--- src/Calendar.php
+++ src/Calendar.php
@@ -1,18 +1,38 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dynamic Calendar JavaScript | CodingNepal</title>
+    <link rel="stylesheet" href="css/Calendar.css">
     <link rel="stylesheet" href="css/LDT.css">
-</head>
-<body>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Font Link for Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+    <script src="Functions/Calendar.js" defer></script>
+  </head>
+  <body>
+    <?php include "Includes/header.html"?>
+    <!-- style = "position:relative; left:-1000px; top:400px;" -->
+    <div class="wrapper">
+      <header>
+        <p class="current-date"></p>
+        <div class="icons">
+          <span id="prev" class="material-symbols-rounded">chevron_left</span>
+          <span id="next" class="material-symbols-rounded">chevron_right</span>
+        </div>
+      </header>
+      <div class="calendar">
+        <ul class="weeks">
+          <li>Sun</li>
+          <li>Mon</li>
+          <li>Tue</li>
+          <li>Wed</li>
+          <li>Thu</li>
+          <li>Fri</li>
+          <li>Sat</li>
+        </ul>
+        <ul class="days"></ul>
+      </div>
+    </div>
     
-<?php
-require('Includes/header.html');
-?>
-
-    
-</body>
+  </body>
 </html>
